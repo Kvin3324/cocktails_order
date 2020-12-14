4009ec7bcf7701d1b3ff278a43582864996a3d50
Kévin Joya <kevinjoya@MacBook-Pro-de-Kevin.local>
2020-12-14 16:52:18 +0100

    Init app routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Link, Switch } from "react-router-dom";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import variables from "./variables";
+import LogginSpace from "./components/LogginSpace/LogginSpace";
+import SignUpSpace from "./components/SignUpSpace/SignUpSpace";
 
 function App() {
+  const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${props => props.theme.backgroundColor};
+  };
+  #root {
+    overflow-x: hidden;
+  };
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={{ ...variables }}>
+        <GlobalStyle></GlobalStyle>
+        <Switch>
+          <Route exact path="/" component={LogginSpace}></Route>
+          <Route exact path="/inscription" component={SignUpSpace}></Route>
+        </Switch>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
