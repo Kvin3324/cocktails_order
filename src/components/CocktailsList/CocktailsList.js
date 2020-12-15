79ff36dfa6765b976ced4819e12ec44ebe8394fc
Kévin <kevin.joya@outlook.com>
2020-12-15 13:05:15 +0100

    Added switch & refacto compo

--- src/components/CocktailsList/CocktailsList.js
+++ src/components/CocktailsList/CocktailsList.js
@@ -1,36 +1,68 @@
 import React from "react";
+import Switch from "@material-ui/core/Switch";
+import CocktailsListStyled from "../../style/CocktailsListStyled.style";
+import CocktailsOrder from "../../CocktailsOrder/CocktailsOrder";
 
 function CocktailsList() {
   const [data, setData] = React.useState({
     cocktails: ["Whisky", "Vodka", "Manzana", "Get", "Ricard", "Vin"],
+    switch: false,
   });
 
+  const handleChange = (event) => {
+    setData({ ...data, [event.target.name]: event.target.checked });
+  };
+
   return (
-    <main>
+    <CocktailsListStyled as="main">
       <section className="about">
         <div className="about--title">
           <h1>Hello Mekton96</h1>
-        </div>
-        <div className="about--text">
-          <p>
-            Tu as envie de te délecter d'une bonne pinte d'alcool ? 🍹🍺 <br />{" "}
-            Fais ton choix !
-          </p>
+          <div className="about--title--switch">
+            <Switch
+              checked={data.switch}
+              onChange={handleChange}
+              color="primary"
+              name="switch"
+              inputProps={{ "aria-label": "primary checkbox" }}
+            />
+            <p>{data.switch ? "Barman 🍹" : "A boire ! 🍻"}</p>
+          </div>
         </div>
       </section>
-      <section className="cocktails--list">
-        {data.cocktails.map((cocktail, index) => {
-					return (
-						<div className="form-check">
-							<input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
-							<label className="form-check-label" for="defaultCheck1">
-								{cocktail}
-							</label>
-						</div>
-					)
-        })}
-      </section>
-    </main>
+      {data.switch ? (
+        <CocktailsOrder></CocktailsOrder>
+      ) : (
+        <>
+          <div className="about--text">
+            <p>
+              Tu as envie de te délecter d'une bonne pinte d'alcool ? 🍹🍺{" "}
+              <br /> Fais ton choix !
+            </p>
+          </div>
+          <section className="cocktails--list">
+            {data.cocktails.map((cocktail, index) => {
+              return (
+                <div className="form-check" key={index}>
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="defaultCheck1"
+                  />
+                  <label className="form-check-label" htmlFor="defaultCheck1">
+                    {cocktail}
+                  </label>
+                </div>
+              );
+            })}
+          </section>
+          <div className="btn--command mt-5">
+            <button className="btn btn-primary">Commander</button>
+          </div>
+        </>
+      )}
+    </CocktailsListStyled>
   );
 }
 
